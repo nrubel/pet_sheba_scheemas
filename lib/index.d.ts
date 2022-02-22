3c613d3c7eb3b9cf73315e7e58048fdc564198b0
Nasir <nasir.rubel99@gmail.com>
2022-02-22 18:20:47 +0600

    on same file

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Model } from "mongoose";
-import { IClinic } from "./schemas/clinic";
-import { IAdmin } from "./schemas/admin";
-export declare const Admin: Model<IAdmin>;
-export declare const Clinic: Model<IClinic>;
--- lib/tools.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const isEmailValid: (val: string) => boolean;
