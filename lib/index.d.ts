cfefc21f65e3dc366cfbbf0af5cf0f391d2e2345
Nasir <nasir.rubel99@gmail.com>
2022-02-22 18:22:12 +0600

    on same file

---
+++ lib/index.d.ts
@@ -0,0 +1,29 @@
+import { Document, Model, Schema } from "mongoose";
+export declare const isEmailValid: (val: string) => boolean;
+export declare const adminRoles: string[];
+export interface IAdmin extends Document {
+    email: string;
+    name: string;
+    password: string;
+    loginCount: number;
+    role: `root` | `admin`;
+}
+export declare const AdminSchema: Schema;
+export declare const Admin: Model<IAdmin>;
+export interface IClinic extends Document {
+    ownerName: string;
+    ownerNIdNo: string;
+    ownerNIdImage: string;
+    clinicName: string;
+    clinicTradeLicenseNo: string;
+    clinicTradeLicenseImage: string;
+    email: string;
+    phone: string;
+    password: string;
+    location: string[];
+    address: string;
+    loginCount: number;
+    logo: string;
+}
+export declare const ClinicSchema: Schema;
+export declare const Clinic: Model<IClinic>;
